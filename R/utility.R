d4fea45be6e0f0fdee53377469e20298145afc93
Alice Cecile <alice.i.cecile@gmail.com>
2014-03-03 14:21:15 -0500

    Added a simple function to truncate tree ring arrays to a minimum sample depth.

--- R/utility.R
+++ R/utility.R
@@ -1,3 +1,16 @@
+# Truncate tree ring array to ensure minimum sample depth ####
+truncate_tra <- function(tra, min_depth=1, id="Time", group_by=NA)
+{
+  depth <- sample_depth_tra(tra, id, group_by)
+    
+  valid_indices <- names(depth[depth>=min_depth])
+  
+  trunc_tra <- tra[tra[[id]] %in% valid_indices,]
+  
+  return(trunc_tra)
+}
+  
+  
 # Geometric mean utility function ####
 geomMean <- function(x){
   if (length(x)==0){
